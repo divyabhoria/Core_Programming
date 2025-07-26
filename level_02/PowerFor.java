05d4a0bfa9c08a6e67b8cdae97671a6c1699f9f6
Divya Bhoria <endemicspecies24@gmail.com>
2025-07-26 22:23:50 +0530

    Update PowerFor.java

--- level_02/PowerFor.java
+++ level_02/PowerFor.java
@@ -1,24 +1,30 @@
 import java.util.Scanner;
 
-public class PowerWhile {
+public class PowerFor {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         System.out.print("Enter base number: ");
         int number = input.nextInt();
+
         System.out.print("Enter power (non-negative): ");
         int power = input.nextInt();
 
         if (power < 0) {
             System.out.println("Invalid power.");
+            input.close();
             return;
         }
 
         int result = 1;
-        int counter = 0;
-        while (counter < power) {
+
+        // Use for loop to compute power
+        for (int i = 1; i <= power; i++) {
             result *= number;
-            counter++;
         }
+
         System.out.println(number + " raised to the power " + power + " is " + result);
+
+        input.close();
     }
 }
